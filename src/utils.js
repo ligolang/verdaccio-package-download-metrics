b2412db2cdfd14685d1ef1f9694bd8f13bc7ca0c
callistonianembrace <87455417+callistonianembrace@users.noreply.github.com>
2022-08-26 16:19:06 +0530

    Adds getWeek utility

---
+++ src/utils.js
@@ -0,0 +1,36 @@
+/**
+ * Returns the week number for timestamp date.  dowOffset is the day of week the week
+ * "starts" on for your locale - it can be from 0 to 6. If dowOffset is 1 (Monday),
+ * the week returned is the ISO 8601 week number.
+ * @param int dowOffset
+ * @return int
+ */
+export function getWeek(timestamp, dowOffset) {
+  dowOffset = typeof dowOffset == 'number' ? dowOffset : 0; //default dowOffset to zero
+  var newYear = new Date(timestamp.getFullYear(), 0, 1);
+  var day = newYear.getDay() - dowOffset; //the day of week the year begins on
+  day = day >= 0 ? day : day + 7;
+  var daynum =
+    Math.floor(
+      (timestamp.getTime() -
+        newYear.getTime() -
+        (timestamp.getTimezoneOffset() - newYear.getTimezoneOffset()) * 60000) /
+        86400000
+    ) + 1;
+  var weeknum;
+  //if the year starts before the middle of a week
+  if (day < 4) {
+    weeknum = Math.floor((daynum + day - 1) / 7) + 1;
+    if (weeknum > 52) {
+      nYear = new Date(timestamp.getFullYear() + 1, 0, 1);
+      nday = nYear.getDay() - dowOffset;
+      nday = nday >= 0 ? nday : nday + 7;
+      /*if the next year starts before the middle of
+              the week, it is week #1 of that year*/
+      weeknum = nday < 4 ? 1 : 53;
+    }
+  } else {
+    weeknum = Math.floor((daynum + day - 1) / 7);
+  }
+  return weeknum;
+}
